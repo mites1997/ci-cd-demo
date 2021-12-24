7fe623333db9efcf77c9a3e99f748a627eda3235
mites1997 <arnabfeb15@gmail.com>
2021-12-24 10:35:36 +0530

    fix the failing test case

--- src/test/java/com/bootcamp/cicddemo/CiCdDemoApplicationTests.java
+++ src/test/java/com/bootcamp/cicddemo/CiCdDemoApplicationTests.java
@@ -1,17 +1,14 @@
 package com.bootcamp.cicddemo;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
-
 
 @SpringBootTest
 class CiCdDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
-		Assertions.assertFalse(true);
+		
 	}
 
 }
